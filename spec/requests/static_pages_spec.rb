5319b9fb7fa1e4ea0004006bc78156cc4625f3d0
letmsee <duong_ubuntu>
2014-01-01 09:30:35 +0700

    Finish layout, routes, and signup pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,34 +1,35 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	describe "Home page" do
-    	it "should have the content SampleApp" do
-    		visit '/static_pages/home'
-    		expect(page).to have_content('Sample App')    		
-    	end
+  subject { page }
 
-    	it "should have the title home" do
-    		visit "/static_pages/home"
-    		expect(page).to have_title('Home')
-    	end
+  describe "Home page" do
+    before { visit root_path }
 
-      it "should have the base title" do
-        visit "/static_pages/home"
-        expect(page).to have_title('Rail SampleApp')
-      end
-  	end
+    it { should have_content('Home') }      
+    it { should have_title(full_title('Home')) }      
+  end
 
-  	describe "Help page" do
-  		it "should have the content Help" do
-  			visit '/static_pages/help'
-  			expect(page).to have_content('Help')
-  		end
-  	end
+  describe "Help page" do
+    before { visit help_path }   
 
-  	describe "About page" do
-  		it "should have the content About" do
-  			visit '/static_pages/about'
-  			expect(page).to have_content('About')
-  		end
-  	end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
+
+
